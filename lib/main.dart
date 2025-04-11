e97071bd3445dba9810ba31aac12ade9cb462df2
your name <you@example.com>
2025-04-11 10:51:56 +0300

    added firebase_core and firebase_messaingging dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(HabitHiveApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:DefaultFirebaseOptions.currentPlatform,
+  ); // Initializes Firebase
+
+  runApp( const HabitHiveApp());
 }
 
 class HabitHiveApp extends StatelessWidget {
+  const HabitHiveApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
